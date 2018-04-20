ff9c138f91c08c1bbc199aa8e7a67eec086d1a62
Habib Hadi <habibullah@augmedix.com>
2018-04-20 14:54:53 +0600

    custom built

--- build-config.js
+++ build-config.js
@@ -16,19 +16,19 @@ module.exports = {
 	plugins: [
 		'@ckeditor/ckeditor5-essentials/src/essentials',
 
-		'@ckeditor/ckeditor5-adapter-ckfinder/src/uploadadapter',
-		'@ckeditor/ckeditor5-autoformat/src/autoformat',
+		// '@ckeditor/ckeditor5-adapter-ckfinder/src/uploadadapter',
+		// '@ckeditor/ckeditor5-autoformat/src/autoformat',
 		'@ckeditor/ckeditor5-basic-styles/src/bold',
 		'@ckeditor/ckeditor5-basic-styles/src/italic',
-		'@ckeditor/ckeditor5-block-quote/src/blockquote',
-		'@ckeditor/ckeditor5-easy-image/src/easyimage',
-		'@ckeditor/ckeditor5-heading/src/heading',
-		'@ckeditor/ckeditor5-image/src/image',
-		'@ckeditor/ckeditor5-image/src/imagecaption',
-		'@ckeditor/ckeditor5-image/src/imagestyle',
-		'@ckeditor/ckeditor5-image/src/imagetoolbar',
-		'@ckeditor/ckeditor5-image/src/imageupload',
-		'@ckeditor/ckeditor5-link/src/link',
+		// '@ckeditor/ckeditor5-block-quote/src/blockquote',
+		// '@ckeditor/ckeditor5-easy-image/src/easyimage',
+		// '@ckeditor/ckeditor5-heading/src/heading',
+		// '@ckeditor/ckeditor5-image/src/image',
+		// '@ckeditor/ckeditor5-image/src/imagecaption',
+		// '@ckeditor/ckeditor5-image/src/imagestyle',
+		// '@ckeditor/ckeditor5-image/src/imagetoolbar',
+		// '@ckeditor/ckeditor5-image/src/imageupload',
+		// '@ckeditor/ckeditor5-link/src/link',
 		'@ckeditor/ckeditor5-list/src/list',
 		'@ckeditor/ckeditor5-paragraph/src/paragraph'
 	],
@@ -37,22 +37,24 @@ module.exports = {
 	config: {
 		toolbar: {
 			items: [
-				'heading',
-				'|',
+				// 'heading',
+				// '|',
 				'bold',
 				'italic',
-				'link',
+				// 'link',
 				'bulletedList',
 				'numberedList',
-				'imageUpload',
-				'blockQuote',
+				// 'imageUpload',
+				// 'blockQuote',
 				'undo',
 				'redo'
 			]
 		},
 
 		image: {
-			toolbar: [ 'imageStyle:full', 'imageStyle:side', '|', 'imageTextAlternative' ]
+			toolbar: [
+				// 'imageStyle:full', 'imageStyle:side', '|', 'imageTextAlternative' 
+			]
 		},
 
 		// UI language. Language codes follow the https://en.wikipedia.org/wiki/ISO_639-1 format.
